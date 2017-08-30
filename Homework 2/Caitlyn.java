03dc655e029f9038e0fa0ba6b202c0db6e0577b6
Caitlyn Brown <caitlyn.brown428@gmail.com>
2017-08-30 12:44:11 -0400

    Hello World hw

---
+++ Homework 2/Caitlyn.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Scott Brady
+   Date Due:   8/26/2017
+ ******************************************************************************/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		// Print out a message to the whole world!
+		System.out.println("Hello World! My name is Caitlyn");
+	}
+}
