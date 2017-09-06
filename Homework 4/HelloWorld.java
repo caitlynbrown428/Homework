c4569fe3adc7d289eb0889dd865339fc18ea6c74
Caitlyn Brown <caitlyn.brown428@gmail.com>
2017-09-06 09:49:47 -0400

    Homework 4
    Hello World with variable

---
+++ Homework 4/HelloWorld.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Scott Brady
+   Date Due:   8/26/2017
+ ******************************************************************************/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		String firstname;
+		String lastname;
+
+		firstname = "caitlyn";
+		lastname = "brown";
+
+		// Print out a message to the whole world!
+		System.out.println(firstname + lastname);
+	}
+}
