0da4fb200725a4193fd81798285e521fd2b0b834
Caitlyn Brown <caitlyn.brown428@gmail.com>
2017-09-26 09:52:00 -0400

    Guessing Game
    Best of luck to you

--- Homework6/GuessingGame.java
+++ Homework6/GuessingGame.java
@@ -2,26 +2,33 @@
  CLASS INFORMATION                                    * Score: ______________ *
  -----------------                                    *************************
    Programmer: Caitlyn Brown
-   Date Due:   9/16/2017
+   Date Due:   9/29/2017
  ******************************************************************************/
 
 import java.util.Scanner;
 
-public class interactive
+public class GuessingGame
 {
 	public static void main(String[] args)
 	{
 		Scanner keyboard = new Scanner(System.in);
 
-		String name;
-		String age;
+		int a;
 
-		System.out.print("What is your name? ");
-		name = keyboard.nextLine();
-		System.out.print("How old are you? ");
-		age = keyboard.nextInt();
-		System.out.printf("Hello, %s! You are %s years old.\n", name, age);
+		System.out.println("Guess what number I'm thinking of. It's between 1 and 10,000. ");
+		a = keyboard.nextInt();
 
-		
+		if (a == 5927)
+		{
+				System.out.println("Well Concered!");
+		}
+		else if (a < 5927 && a > 0)
+		{
+				System.out.println("Too Low...");
+		}
+		else if (a > 5927)
+		{
+				System.out.println("Too High...");
+		}
 	}
 }
