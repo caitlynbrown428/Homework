eb3ee8758f7a8698ddca56d57a0366d7838cf087
Caitlyn Brown <caitlyn.brown428@gmail.com>
2017-09-27 08:59:13 -0400

    Updated Homework 6!!!!

--- Homework6/GuessingGame.java
+++ Homework6/GuessingGame.java
@@ -18,15 +18,15 @@ public class GuessingGame
 		System.out.println("Guess what number I'm thinking of. It's between 1 and 10,000. ");
 		a = keyboard.nextInt();
 
-		if (a == 5927)
+		if (a == 1234)
 		{
-				System.out.println("Well Concered!");
+				System.out.println("Well Conquered!");
 		}
-		else if (a < 5927 && a > 0)
+		else if (a < 1234 && a > 0)
 		{
 				System.out.println("Too Low...");
 		}
-		else if (a > 5927)
+		else if (a > 1234)
 		{
 				System.out.println("Too High...");
 		}
