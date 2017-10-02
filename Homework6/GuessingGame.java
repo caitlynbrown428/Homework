dc25498db76f2218074bd47f47480236bce6fbe1
Caitlyn Brown <caitlyn.brown428@gmail.com>
2017-10-02 10:57:22 -0400

    Guessing Game with while loop

--- Homework6/GuessingGame.java
+++ Homework6/GuessingGame.java
@@ -18,17 +18,16 @@ public class GuessingGame
 		System.out.println("Guess what number I'm thinking of. It's between 1 and 10,000. ");
 		a = keyboard.nextInt();
 
-		if (a == 1234)
+		while(a< 1234 && a > 0)
 		{
-				System.out.println("Well Conquered!");
+			System.out.println("Too Low...");
+			a = keyboard.nextInt();
 		}
-		else if (a < 1234 && a > 0)
+		while(a > 1234)
 		{
-				System.out.println("Too Low...");
+			System.out.println("Too High...");
+			a = keyboard.nextInt();
 		}
-		else if (a > 1234)
-		{
-				System.out.println("Too High...");
-		}
+		System.out.println("Well Conquered!");
 	}
 }
--- Homework7/divisible.java
+++ Homework7/divisible.java
@@ -20,9 +20,7 @@ public class divisible
 
 		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
 		{
-			{
 				System.out.println("It's a leap year!");
-			}
 		}
 		else
 		{
