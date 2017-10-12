d9b7b1376a9c32f2d7447dfa6edc76baa1b75e55
Caitlyn Brown <caitlyn.brown428@gmail.com>
2017-10-12 12:12:19 -0400

    Homework 8

---
+++ Homework8/NegativeNumber.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+ CLASS INFORMATION                                    * Score: ______________ *
+ -----------------                                    *************************
+   Programmer: Caitlyn Brown
+   Date Due:   10/13/2017
+ ******************************************************************************/
+
+import java.util.Scanner;
+
+public class NegativeNumber
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		int num;
+		int sum = 0;
+		int largest = num;
+
+		System.out.println("Enter a positive number or a negative number to stop: ");
+		num = keyboard.nextInt();
+
+		while (num >= 0)
+		{
+			sum += num;
+			System.out.println("That's positive. Give me a negative number to stop: ");
+			num = keyboard.nextInt();
+			if (num > largest)
+			{
+				largest = num;
+			}
+		}
+		System.out.println("That's negative!");
+		System.out.println("The sum of the positive numbers is " + sum);
+		System.out.println(largest + " is the largest number you typed."); 
+	}
+}
